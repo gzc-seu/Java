032deee0e5ba09b77df99321673fe61f04219d0f
gzc_seu <974169095@qq.com>
2020-01-11 12:05:51 +0800

    mybatis框架学习第二天

---
+++ Mybatis/Mybatis-Study-01/day03_eesy_03annoOne2Many/src/main/java/com/gzc/dao/IUserDao.java
@@ -0,0 +1,40 @@
+package com.gzc.dao;
+
+import com.gzc.domain.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+@CacheNamespace(blocking =true)  //开启二级缓存
+public interface IUserDao {
+    /**
+     * 查询所有用户
+     * @return
+     */
+    @Select("select * from user")
+    @Results(id="userMap",value={
+            @Result(id=true,column = "id",property = "userId"),
+            @Result(column = "username",property = "userName"),
+            @Result(column = "address",property = "userAddress"),
+            @Result(column = "sex",property = "userSex"),
+            @Result(column = "birthday",property = "userBirthday")
+    })
+    List<User> findAll();
+
+    /**
+     * 根据id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user  where id=#{id} ")
+    @ResultMap("userMap")
+    User findById(Integer userId);
+
+    /**
+     * 根据用户名称模糊查询
+     * @param username
+     * @return
+     */
+    @Select("select * from user where username like #{username} ")
+    List<User> findUserByName(String username);
+
+}
---
+++ Mybatis/Mybatis-Study-01/day03_eesy_03annoOne2Many/src/test/java/com/gzc/test/AnnotationCRUDTest.java
@@ -0,0 +1,55 @@
+package com.gzc.test;
+
+import com.gzc.dao.IUserDao;
+import com.gzc.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+
+public class AnnotationCRUDTest {
+    private InputStream in;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+    private IUserDao userDao;
+
+    @Before
+    public  void init()throws Exception{
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        factory = new SqlSessionFactoryBuilder().build(in);
+        session = factory.openSession();
+        userDao = session.getMapper(IUserDao.class);
+    }
+
+    @After
+    public  void destroy()throws  Exception{
+        session.commit();
+        session.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindOne(){
+        User user = userDao.findById(57);
+        System.out.println(user);
+    }
+
+
+    @Test
+    public  void testFindByName(){
+        List<User> users = userDao.findUserByName("%mybatis%");
+        //List<User> users = userDao.findUserByName("mybatis");
+        for(User user : users){
+            System.out.println(user);
+        }
+    }
+
+}
